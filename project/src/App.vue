d28a9d02317a589c5aa9daee2fc82d20f06b91e2
kaizei <wangkai@zxunity.com>
2022-07-21 16:14:20 +0800

    add vue-router & element-ui.

--- project/src/App.vue
+++ project/src/App.vue
@@ -7,12 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <el-button type="primary">导航</el-button>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
